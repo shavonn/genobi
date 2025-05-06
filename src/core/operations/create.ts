95f2e1cc3cf7d9a101d9ddcd7f79a58d1e76bdc0
Shavonn Brown <shavonn@sysmatter.com>
2025-05-06 13:41:16 -0400

    Add create op and tests

--- src/core/operations/create.ts
+++ src/core/operations/create.ts
@@ -1,7 +1,39 @@
+import fs from "node:fs/promises";
+import path from "node:path";
 import type { CreateOperation } from "../../types/operation";
+import { content } from "../../utils/content";
+import { logger } from "../../utils/logger";
+import { pathDir } from "../../utils/path-dir";
+import { templateProcessor } from "../../utils/template-processor";
 
 async function create(operation: CreateOperation, data: Record<string, any>): Promise<void> {
-	console.log("Create operation...", operation, data);
+	const filePath = pathDir.getTemplateProcessedPath(operation.filePath, data);
+
+	await pathDir.ensureDirectoryExists(path.dirname(filePath));
+
+	const exists = await pathDir.fileExists(filePath);
+	if (exists) {
+		if (operation.overwrite) {
+			logger.warn(`File already exists: ${filePath}. Overwriting.`);
+		} else if (operation.skipIfExists) {
+			logger.warn(`File already exists: ${filePath}. Skipping.`);
+			return;
+		} else {
+			throw new Error(`File already exists: ${filePath}`);
+		}
+	}
+
+	const processedContent = await content.getSingleFileContent(operation, data).then((content) => {
+		return templateProcessor.process(content, data);
+	});
+
+	try {
+		await fs.writeFile(filePath, processedContent);
+		logger.success(`Created file: ${filePath}`);
+	} catch (error) {
+		logger.error(`Error writing file: ${filePath}`);
+		throw error;
+	}
 }
 
 export { create };
---
+++ tests/core/operations/create.test.ts
@@ -0,0 +1,125 @@
+import fs from "node:fs/promises";
+import { operations } from "../../../src/core/operations";
+import { content } from "../../../src/utils/content";
+import { helperRegister } from "../../../src/utils/helpers/helper-register";
+import { logger } from "../../../src/utils/logger";
+import { pathDir } from "../../../src/utils/path-dir";
+import { testData } from "../../__fixtures__/test-data";
+import { testFiles } from "../../__fixtures__/test-files";
+import { getTmpDirPath, loadTestFiles, writeTestFile } from "../../test-utils";
+
+describe("create", () => {
+	let componentFilePath: string;
+
+	const mergedData = {
+		name: "page header",
+		...testData.themeData,
+	};
+
+	beforeAll(async () => {
+		helperRegister.register();
+	});
+
+	beforeEach(async () => {
+		componentFilePath = getTmpDirPath("src/components/page-header/page-header.tsx");
+
+		await loadTestFiles(testFiles.existingFiles);
+	});
+
+	it("should create file using templateFile", async () => {
+		const operation = testData.makeCreateOperation();
+
+		await operations.create(operation, mergedData);
+
+		const fileResult = await fs.readFile(componentFilePath, "utf8");
+		expect(fileResult).toContain("PageHeader()");
+		expect(fileResult).toContain("denim-page-header");
+	});
+
+	it("should create a file using templateStr", async () => {
+		const operation = testData.makeCreateOperation({
+			templateFilePath: undefined,
+			filePath: testFiles.componentCss.filePath,
+			templateStr: testFiles.componentCss.templateStr,
+		});
+		const cssFilePath = getTmpDirPath("src/components/page-header/page-header.css");
+
+		await operations.create(operation, mergedData);
+
+		const fileResult = await fs.readFile(cssFilePath, "utf8");
+		expect(fileResult).toContain(".page-header {");
+		expect(fileResult).toContain("background-color: denim-600;");
+	});
+
+	it("should throw error if file already exists and skipIfExists is false", async () => {
+		const operation = testData.makeCreateOperation({
+			templateFilePath: undefined,
+		});
+
+		vi.spyOn(fs, "readFile");
+
+		await loadTestFiles({
+			"src/components/page-header/page-header.tsx": "page header",
+		});
+
+		vi.spyOn(pathDir, "fileExists");
+
+		await expect(operations.create(operation, mergedData)).rejects.toThrow();
+
+		expect(pathDir.fileExists).toHaveResolvedWith(true);
+		expect(fs.readFile).not.toHaveBeenCalled();
+	});
+
+	it("should skip operation if file already exists and skipIfExists is true", async () => {
+		const operation = testData.makeCreateOperation({
+			skipIfExists: true,
+		});
+
+		vi.spyOn(content, "getSingleFileContent");
+
+		await loadTestFiles({
+			"src/components/page-header/page-header.tsx": "page header",
+		});
+
+		await operations.create(operation, mergedData);
+
+		expect(pathDir.fileExists).toHaveResolvedWith(true);
+		expect(content.getSingleFileContent).not.toHaveBeenCalled();
+	});
+
+	it("should overwrite file if file already exists and overwrite is true", async () => {
+		const operation = testData.makeCreateOperation({
+			templateFilePath: undefined,
+			templateStr: testFiles.component.templateFileContent,
+			overwrite: true,
+		});
+		const mergedData = {
+			name: "alert",
+			...testData.themeData,
+		};
+		const alertFilePath = getTmpDirPath("src/components/alert/alert.tsx");
+
+		await writeTestFile("src/components/alert/alert.tsx", "to be overwritten as alert component");
+
+		const existingFileResult = await fs.readFile(alertFilePath, "utf8");
+		expect(existingFileResult).toContain("overwritten");
+
+		await operations.create(operation, mergedData);
+
+		const newContentResult = await fs.readFile(alertFilePath, "utf8");
+		expect(newContentResult).not.toContain("overwritten");
+
+		expect(pathDir.fileExists).toHaveResolvedWith(true);
+		expect(content.getSingleFileContent).toHaveBeenCalled();
+	});
+
+	it("should throw error when writing file fails", async () => {
+		const operation = testData.makeCreateOperation();
+
+		vi.spyOn(fs, "writeFile").mockRejectedValueOnce(new Error());
+
+		await expect(operations.create(operation, mergedData)).rejects.toThrow();
+
+		expect(logger.error).toHaveBeenCalledWith(expect.stringContaining("Error writing file"));
+	});
+});

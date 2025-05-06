fc2ab4b5c37f5ef499157de9df092414ae2c9a52
Shavonn Brown <shavonn@sysmatter.com>
2025-05-06 09:45:37 -0400

    Add operations to type exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
 import type { HelperDelegate } from "handlebars";
+import type { DistinctQuestion } from "inquirer";
 import type { ConfigAPI } from "./types/config-api";
+import type { GeneratorConfig } from "./types/generator";
+import type {
+	AmendOperation,
+	AppendOperation,
+	CreateAllOperation,
+	CreateOperation,
+	Operation,
+	PrependOperation,
+} from "./types/operation";
 
-export type { ConfigAPI, HelperDelegate };
+export type {
+	ConfigAPI,
+	HelperDelegate,
+	DistinctQuestion,
+	GeneratorConfig,
+	Operation,
+	AmendOperation,
+	AppendOperation,
+	CreateOperation,
+	CreateAllOperation,
+	PrependOperation,
+};

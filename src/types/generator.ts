1f550d14247e05f94e287bac8f44b1255badbda9
Shavonn Brown <shavonn@sysmatter.com>
2025-05-06 08:47:45 -0400

    Add prompts and operations

--- src/types/generator.ts
+++ src/types/generator.ts
@@ -1,3 +1,8 @@
+import type { DistinctQuestion } from "inquirer";
+import type { Operation } from "./operation";
+
 export interface GeneratorConfig {
 	description: string;
+	prompts: DistinctQuestion[];
+	operations: Operation[];
 }
---
+++ src/types/operation.ts
@@ -0,0 +1,45 @@
+export type Operation = AmendOperation | CreateOperation | CreateAllOperation;
+
+export interface BaseOperation {
+	type: string;
+	data?: Record<string, any>;
+	skip?: (data?: any) => boolean;
+	haltOnError?: boolean;
+}
+
+export interface SingleFileOperation extends BaseOperation {
+	filePath: string;
+	templateStr?: string;
+	templateFilePath?: string;
+}
+
+export interface AmendOperation extends SingleFileOperation {
+	type: "append" | "prepend";
+	separator?: string;
+	unique?: boolean;
+	pattern?: string | RegExp;
+}
+
+export interface AppendOperation extends AmendOperation {
+	type: "append";
+}
+
+export interface PrependOperation extends AmendOperation {
+	type: "prepend";
+}
+
+export interface CreateOperation extends SingleFileOperation {
+	type: "create";
+	skipIfExists?: boolean;
+	overwrite?: boolean;
+}
+
+export interface CreateAllOperation extends BaseOperation {
+	type: "createAll";
+	destinationPath: string;
+	templateFileGlob: string;
+	templateBasePath?: string;
+	verbose?: boolean;
+	skipIfExists?: boolean;
+	overwrite?: boolean;
+}

f27e2b784901ee2b2bc31b2a04141a67d4434f97
Shavonn Brown <shavonn@sysmatter.com>
2025-05-07 13:27:01 -0400

    Test operation fail ends process

--- tests/core/client-runner.test.ts
+++ tests/core/client-runner.test.ts
@@ -1,4 +1,8 @@
+import fs from "node:fs/promises";
 import { Command } from "commander";
+import inquirer from "inquirer";
+import { expect } from "vitest";
+import { configAPI } from "../../src/config-api";
 import { store } from "../../src/config-store";
 import { cli } from "../../src/core/client-runner";
 import { configLoader } from "../../src/core/config-loader";
@@ -6,45 +10,74 @@ import { generatorResolver } from "../../src/core/generator-resolver";
 import { generatorRunner } from "../../src/core/generator-runner";
 import { logger } from "../../src/utils/logger";
 import { testData } from "../__fixtures__/test-data";
+import { testFiles } from "../__fixtures__/test-files";
+import { loadTestFiles } from "../test-utils";
 
 describe("runCli", () => {
-	beforeEach(() => {
-		vi.spyOn(configLoader, "load").mockResolvedValueOnce();
-		vi.spyOn(generatorResolver, "resolve").mockResolvedValueOnce();
-		vi.spyOn(generatorRunner, "run").mockResolvedValueOnce();
+	describe("mocked core functions", () => {
+		beforeEach(() => {
+			vi.spyOn(configLoader, "load").mockResolvedValueOnce();
+			vi.spyOn(generatorResolver, "resolve").mockResolvedValueOnce();
+			vi.spyOn(generatorRunner, "run").mockResolvedValueOnce();
+		});
+
+		it("should setup commander and load config", async () => {
+			vi.spyOn(Command.prototype, "version");
+			vi.spyOn(Command.prototype, "description");
+			vi.spyOn(Command.prototype, "parse");
+
+			await cli.run();
+
+			expect(Command.prototype.version).toHaveBeenCalled();
+			expect(Command.prototype.description).toHaveBeenCalled();
+			expect(Command.prototype.parse).toHaveBeenCalled();
+			expect(logger.error).not.toHaveBeenCalled();
+		});
+
+		it("should exit process when an error is thrown from core function", async () => {
+			vi.spyOn(configLoader, "load").mockRejectedValueOnce(
+				new Error("Config file not found. Create one to define your generators, helpers, and other options."),
+			);
+
+			await expect(cli.run()).rejects.toThrow('process.exit unexpectedly called with "1"');
+			expect(logger.error).toHaveBeenCalledWith(
+				"Error: Config file not found. Create one to define your generators, helpers, and other options.",
+			);
+		});
+
+		it("should use generator arg when provided", async () => {
+			vi.spyOn(process, "argv", "get").mockReturnValueOnce(["", "", testData.component.id]);
+			vi.spyOn(store, "setSelectedGenerator");
+
+			await cli.run();
+
+			expect(store.setSelectedGenerator).toHaveBeenCalledWith(testData.component.id);
+			expect(logger.error).not.toHaveBeenCalled();
+		});
 	});
 
-	it("should setup commander and load config", async () => {
-		vi.spyOn(Command.prototype, "version");
-		vi.spyOn(Command.prototype, "description");
-		vi.spyOn(Command.prototype, "parse");
+	describe("actual generator runner", () => {
+		beforeEach(async () => {
+			await loadTestFiles(testFiles.existingFiles);
+		});
 
-		await cli.run();
+		it("should exit process when an error is thrown from operation", async () => {
+			testData.slimConfigFunc(configAPI.get());
+			store.setSelectedGenerator(testData.component.id);
+			const input = { name: "table cell" };
 
-		expect(Command.prototype.version).toHaveBeenCalled();
-		expect(Command.prototype.description).toHaveBeenCalled();
-		expect(Command.prototype.parse).toHaveBeenCalled();
-		expect(logger.error).not.toHaveBeenCalled();
-	});
+			vi.spyOn(process, "argv", "get").mockReturnValue(["", "", testData.component.id]);
+			vi.spyOn(configLoader, "load").mockImplementationOnce(vi.fn());
+			vi.spyOn(generatorResolver, "resolve").mockImplementationOnce(vi.fn());
+			vi.spyOn(inquirer, "prompt").mockResolvedValueOnce(input);
+			vi.spyOn(fs, "writeFile").mockRejectedValueOnce(new Error());
 
-	it("should exit process when an error is thrown", async () => {
-		vi.spyOn(configLoader, "load").mockRejectedValueOnce(
-			new Error("Config file not found. Create one to define your generators, helpers, and other options."),
-		);
+			await expect(cli.run()).rejects.toThrow('process.exit unexpectedly called with "1"');
 
-		await expect(cli.run()).rejects.toThrow('process.exit unexpectedly called with "1"');
-		expect(logger.error).toHaveBeenCalledWith(
-			"Error: Config file not found. Create one to define your generators, helpers, and other options.",
-		);
-	});
-
-	it("should use generator arg when provided", async () => {
-		vi.spyOn(process, "argv", "get").mockReturnValueOnce(["", "", testData.component.id]);
-		vi.spyOn(store, "setSelectedGenerator");
-
-		await cli.run();
-
-		expect(store.setSelectedGenerator).toHaveBeenCalledWith(testData.component.id);
-		expect(logger.error).not.toHaveBeenCalled();
+			expect(logger.error).toHaveBeenCalledWith(
+				expect.stringContaining("Create operation failed"),
+				expect.stringContaining("Error writing file"),
+			);
+		});
 	});
 });

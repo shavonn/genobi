043b34f122b6305600e891d2cf42cbe53c5a9ef3
Shavonn Brown <shavonn@sysmatter.com>
2025-05-09 16:26:10 -0400

    Use search strategy for installation type

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,8 +1,22 @@
+import { execSync } from "node:child_process";
+
 function escapeRegExp(str: string): string {
 	return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
+function isGlobalInstall() {
+	try {
+		const globalNodeModules = execSync("npm root -g").toString().trim();
+		const currentPath = __dirname;
+
+		return currentPath.startsWith(globalNodeModules);
+	} catch (_err: any) {
+		return false;
+	}
+}
+
 const common = {
 	escapeRegExp,
+	isGlobalInstall,
 };
 export { common };

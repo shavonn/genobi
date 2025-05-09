551fca0e47d1f57fd02d1e84d77e4120ea3bc0d5
Shavonn Brown <shavonn@sysmatter.com>
2025-05-09 09:24:52 -0400

    Load existing files func

--- tests/setup-tmpdir.ts
+++ tests/setup-tmpdir.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { store } from "../src/config-store";
 import { testData } from "./__fixtures__/test-data";
+import { testFiles } from "./__fixtures__/test-files";
 import { cleanUpTmpDir, createTmpDir, getTmpDir } from "./test-utils";
 
 beforeEach(async () => {
@@ -10,6 +11,7 @@ beforeEach(async () => {
 	const dir = getTmpDir();
 	store.setDestinationBasePath(dir);
 	store.setConfigFilePath(path.resolve(dir, testData.configFilePath));
+	await testFiles.loadExistingFiles();
 });
 
 afterEach(async () => {
